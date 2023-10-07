a6ab7f24456e14a7fa0f46ba330bf0ba33426ddc
Ana Livia Oliveira <143213393+livia0oliveira@users.noreply.github.com>
2023-10-06 22:54:22 -0300

    criada a pasta de games

---
+++ game projects/first time - 2D game/GameFirstTry/src/leMain/First.java
@@ -0,0 +1,22 @@
+package leMain;
+
+import javax.swing.JFrame;
+
+public class First {
+	public static void main(String[] args) {
+		
+		JFrame window = new JFrame();
+		
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Cool game :D");
+		
+		GamePanel gamePanel = new GamePanel();
+		window.add(gamePanel);
+		window.pack();
+		
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+	}
+
+}
---
+++ game projects/first time - 2D game/GameFirstTry/src/leMain/GamePanel.java
@@ -0,0 +1,28 @@
+package leMain;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel {
+
+	//le screen settings
+	
+	final int OriginalTileSize = 16; //16x16 tile
+	final int scale = 3;
+	
+	final int TileSize = OriginalTileSize * scale; // 48 tile soize
+	final int maxWindowCol = 19;
+	final int maxWindowRow = 13;
+	final int screenWidth = maxWindowCol * TileSize; // 768 pixels
+	final int screenHeight = maxWindowRow * TileSize; // 624 pixels
+	
+	public GamePanel() {
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		
+	}
+	
+}
